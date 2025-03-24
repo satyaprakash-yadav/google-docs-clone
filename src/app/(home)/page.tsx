46d53ba54f0f62df3a577612f68e4a9fbba8c8d9
satyaprakash-yadav <satyay9798@gmail.com>
2025-03-24 21:07:43 +0530

    feat: Database setup (Convex) Added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,13 @@
+'use client';
+
+import { useQuery } from "convex/react";
 import { Navbar } from "./navbar";
 import { TemplatesGallery } from "./templates-gallery";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -9,6 +15,9 @@ const Home = () => {
       </div>
       <div className="mt-16">
        <TemplatesGallery />
+       {documents?.map((document) => (
+         <span key={document._id}>{document.title}</span>
+       ))}
       </div>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 import "./globals.css";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,7 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning className={inter.className}>
-        <NuqsAdapter>{children}</NuqsAdapter>
+        <NuqsAdapter>
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
