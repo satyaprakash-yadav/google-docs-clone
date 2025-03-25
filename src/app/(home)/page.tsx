c9fbd83590a563c6f0be91e4d829a64dc5c8f47d
satyaprakash-yadav <satyay9798@gmail.com>
2025-03-25 12:47:14 +0530

    feat: Search documents Added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,11 +7,14 @@ import { TemplatesGallery } from "./templates-gallery";
 
 import { Navbar } from "./navbar";
 import { DocumentsTable } from "./documents-table";
+import { useSearchParam } from "@/hooks/use-search-param";
 
 const Home = () => {
+  const [search] = useSearchParam();
+
   const { results, status, loadMore } = usePaginatedQuery(
     api.documents.get,
-    {},
+    { search },
     { initialNumItems: 5 }
   );
 
