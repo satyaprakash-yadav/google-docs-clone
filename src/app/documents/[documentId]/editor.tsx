78789a7f22dfe787176bcbadbd180667fc85ae3a
satyaprakash-yadav <satyay9798@gmail.com>
2025-03-26 13:49:08 +0530

    feat: Collaborative margin ruler Added

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -19,6 +19,7 @@ import { Color } from "@tiptap/extension-color";
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import { useLiveblocksExtension } from "@liveblocks/react-tiptap";
+import { useStorage } from "@liveblocks/react";
 
 import { useEditorStore } from "@/store/use-editor-store";
 import { FontSizeExtension } from "@/extensions/font-size";
@@ -27,6 +28,9 @@ import { Ruler } from "./ruler";
 import { Threads } from "./threads";
 
 export const Editor = () => {
+  const leftMargin = useStorage((root) => root.leftMargin);
+  const rightMargin = useStorage((root) => root.rightMargin);
+
   const liveblocks = useLiveblocksExtension();
   const { setEditor } = useEditorStore();
 
@@ -58,7 +62,7 @@ export const Editor = () => {
     },
     editorProps: {
       attributes: {
-        style: "padding-left: 56px; padding-right: 56px;",
+        style: `padding-left: ${leftMargin ?? 56}px; padding-right: ${rightMargin ?? 56}px;`,
         class:
           "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
